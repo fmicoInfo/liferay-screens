68aeacac57e17646ce21468eaea688e0b63eddde
Francisco Micó <fmico@fmico.info>
2017-08-31 14:57:09 +0200

    LSR-1026 Add AssetDisplay for Showcase-Android

---
+++ xamarin/Samples/Showcase-Android/AssetDisplayActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using Com.Liferay.Mobile.Screens.Asset;
+using Com.Liferay.Mobile.Screens.Asset.Display;
+
+namespace ShowcaseAndroid
+{
+    [Activity]
+    public class AssetDisplayActivity : Activity, IAssetDisplayListener
+    {
+
+        private AssetDisplayScreenlet assetDisplayScreenlet;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.AssetDisplayView);
+
+            assetDisplayScreenlet = (AssetDisplayScreenlet)FindViewById(Resource.Id.asset_display_screenlet);
+            assetDisplayScreenlet.SetListener(this);
+        }
+
+		public void Error(Java.Lang.Exception p0, string p1)
+		{
+			System.Diagnostics.Debug.WriteLine($"Asset display error: {p0}");
+		}
+
+		public void OnRetrieveAssetSuccess(AssetEntry p0)
+		{
+			System.Diagnostics.Debug.WriteLine($"Asset List succes: {p0}");
+		}
+
+    }
+}
